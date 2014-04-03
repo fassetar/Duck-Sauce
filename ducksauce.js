bad0c8003bb85253410688a4a46b5281198c0895
fassetar <fassetar@gmail.com>
2014-04-02 22:00:52 -0400

    Code Check for Build Test 2

--- ducksauce.js
+++ ducksauce.js
@@ -1,18 +1,19 @@
 var fs = require("fs");
 //http://stackoverflow.com/a/13543706/1265036
 var fileName = __dirname +'\\Example.txt';
+console.log(fileName);
 // fs.readFile(fileName, { encoding: "utf8", flag: 'r'}, function (err, data) {
 			// if (err) throw err;
 		// console.log(data);
 		// fs.close(fileName);
 	// });
-fs.watch(fileName,{presistent:true, interval: 10},  function (event, filename) { 
-		fs.readFile(fileName, { encoding: "utf8", flag: 'r'}, function (err, data) {
-			if (err) throw err;
-		console.log(data);
-		//fs.close(fileName);
-	});
-});
+// fs.watch(fileName,{presistent:true, interval: 10},  function (event, filename) { 
+		// fs.readFile(fileName, { encoding: "utf8", flag: 'r'}, function (err, data) {
+			// if (err) throw err;
+		// console.log(data);
+		// //fs.close(fileName);
+	// });
+// });
 		
 //Resources:
 //Travis Build Test: http://stackoverflow.com/a/15734041/1265036
