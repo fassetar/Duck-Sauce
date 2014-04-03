40e0dc5a61d867b7f9d276b1296968186ebb986d
fassetar <fassetar@gmail.com>
2014-04-02 21:44:09 -0400

    File Example

--- ducksauce.js
+++ ducksauce.js
@@ -1,15 +1,16 @@
 var fs = require("fs");
-var fileName = 'Example.txt';
-fs.readFile(fileName, { encoding: "utf8", flag: 'r'}, function (err, data) {
-			if (err) throw err;
-		console.log(data);
-		fs.close(fileName);
-	});
+var fileName = __dirname +'\\Example.txt';
+// fs.readFile(fileName, { encoding: "utf8", flag: 'r'}, function (err, data) {
+			// if (err) throw err;
+		// console.log(data);
+		// fs.close(fileName);
+	// });
 fs.watch(fileName,{presistent:true, interval: 10},  function (event, filename) { 
 		fs.readFile(fileName, { encoding: "utf8", flag: 'r'}, function (err, data) {
 			if (err) throw err;
 		console.log(data);
-		fs.close(fileName);
+		//fs.close(fileName);
 	});
-		
+
 });
+		
