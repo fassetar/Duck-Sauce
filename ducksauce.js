634f06d2514c25a7bfd3a1f874e80c78ff3ee35e
fassetar <fassetar@gmail.com>
2014-04-07 22:28:49 -0400

    Checking if exists

--- ducksauce.js
+++ ducksauce.js
@@ -1,19 +1,25 @@
 var http = require('http'),
     fs = require('fs'), open = require('open');
 
+var index = __dirname +'/index.html';
 //http://stackoverflow.com/a/13543706/1265036
 //var fileName = __dirname +'\\Example.txt';
-fs.readFile(__dirname +'/index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+fs.exists(index, function(exists) {
+    if (exists) {
+        // Do something
+		fs.readFile(index, function (err, html) {
+			if (err) {
+				throw err; 
+			}       
+			http.createServer(function(request, response) {  
+				response.writeHeader(200, {"Content-Type": "text/html"});  
+				response.write(html);  
+				response.end();  
+			}).listen(8000);
+		});
+	}
 });
-console.log(__dirname +'/index.html');
+console.log(index);
 // open('http://localhost:8000', function (err) {
   // if (err) throw err;
   // console.log('The user closed the browser');
