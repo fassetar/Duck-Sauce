4f5d85475c3b236c27215c0a1d33cd5a0821638b
Anthony Fassett <fassetar@gmail.com>
2014-11-03 13:05:35 -0500

    File cleanup and outlined file names

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,51 +3,51 @@ module.exports = function(grunt) {
   grunt.registerTask('watch', [ 'watch' ]);
  
   grunt.initConfig({
-    // concat: {
-      // js: {
-        // options: {
-          // separator: ';'
-        // },
-        // src: [
-          // 'Example/javascript/*.js'
-        // ],
-        // dest: 'Example/javascripts/main.min.js'
-      // },
-    // },
-    // uglify: {
-      // options: {
-        // mangle: false,
+	// concat: {
+	  // js: {
+		// options: {
+		  // separator: ';'
+		// },
+		// src: [
+		  // 'Example/javascript/*.js'
+		// ],
+		// dest: 'Example/javascripts/main.min.js'
+	  // },
+	// },
+	// uglify: {
+	  // options: {
+		// mangle: false,
 		// compress: true,
-      // },
-      // js: {
-        // files: {
-          // 'Example/javascripts/main.min.js': ['Example/javascripts/main.js']
-        // }
-      // }
-    // },
-    // less: {
-      // style: {
-        // files: {
-          // "public/css/style.css": "less/style.less"
-        // }
-      // }
-    // },
-    watch: {
-      js: {
-        files: ["../Scripts/*.js"],
-        //tasks: ['concat:js', 'uglify:js'],
-        options: {
-          livereload: true,
-        }
-      },
-       css: {
-         files: ['less/*.less'],
-         tasks: ['less:style'],
-         options: {
-           livereload: true,
-         }
-       }
-    }
+	  // },
+	  // js: {
+		// files: {
+		  // 'Example/javascripts/main.min.js': ['Example/javascripts/main.js']
+		// }
+	  // }
+	// },
+	// less: {
+	  // style: {
+		// files: {
+		  // "public/css/style.css": "less/style.less"
+		// }
+	  // }
+	// },
+	watch: {
+	  js: {
+		files: ["../Scripts/*.js"],
+		//tasks: ['concat:js', 'uglify:js'],
+		options: {
+		  livereload: true,
+		}
+	  },
+	   css: {
+		 files: ['less/*.less'],
+		 tasks: ['less:style'],
+		 options: {
+		   livereload: true,
+		 }
+	   }
+	}
   });
  
   //grunt.loadNpmTasks('grunt-contrib-concat');
--- devtools-chrome.js
+++
@@ -1,7 +0,0 @@
-var panels = chrome.devtools.panels;
-
-// Grunt panel
-var gruntPanel = panels.create(
-  "Ducksauce",
-  "panel.html"
-);
