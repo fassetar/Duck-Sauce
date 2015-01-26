b155b3c22fc65c1f071e81e8a7ae9d2030041217
Anthony Fassett <fassetar@gmail.com>
2015-01-26 01:00:20 -0500

    Temp code, might revert from here.

---
+++ background.js
@@ -0,0 +1,18 @@
+var activeListeners = {};
+chrome.extension.onConnect.addListener(function (port) {
+    port.onMessage.addListener(function (message) {
+        console.log(message[1]);
+        if (message[0] === 'connect') {
+            // get the tab id
+            var tabId = message[1];
+            
+            // save the reference to the port
+            activeListeners[tabId] = port;
+            
+            // make sure we clean up after disconnecting (closing the panel)
+            activeListeners[tabId].onDisconnect.addListener(function () {
+                delete activeListeners[tabId];
+            });            
+        }
+    });
+});
